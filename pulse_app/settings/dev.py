9547d618f2040a7a00923af8f4377b303650ff70
GuyMonagan <werevalkury@gmail.com>
2026-01-27 15:03:14 +0400

    refactor: вынес настройки в отдельные модули settings/{base,dev,prod}

---
+++ pulse_app/settings/dev.py
@@ -0,0 +1,12 @@
+from .base import *
+
+# Всё, что специфично для разработки:
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
